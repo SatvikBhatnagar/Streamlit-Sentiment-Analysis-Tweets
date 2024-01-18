3065f54495c07b2e0486206ed9739848a02ac6a3
stoner69 <16363598+SatvikBhatnagar@users.noreply.github.com>
2024-01-18 16:37:40 +0530

    plot number of tweets by sentiment for each airline

--- app.py
+++ app.py
@@ -23,12 +23,12 @@ data = load_data()
 
 st.sidebar.subheader("Show random tweet")
 random_tweet = st.sidebar.radio('Sentiment', ('positive', 'neutral', 'negative'))
-st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0,0])
+st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0, 0])
 
 st.sidebar.markdown("### Number of tweets by sentiment")
 select = st.sidebar.selectbox('Visualization Type', ['Histogram', 'Pie chart'], key='1')
 sentiment_count = data['airline_sentiment'].value_counts()
-sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
+sentiment_count = pd.DataFrame({'Sentiment': sentiment_count.index, 'Tweets': sentiment_count.values})
 
 if not st.sidebar.checkbox("Hide", True):
     st.markdown("### Number of tweets by sentiment")
@@ -45,7 +45,18 @@ hour = st.sidebar.slider("Hour of day", 0, 23)
 modified_data = data[data['tweet_created'].dt.hour == hour]
 if not st.sidebar.checkbox("Close", True, key='2'):
     st.markdown("### Tweets locations based on the time of day")
-    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour+1)%24))
+    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
     st.map(modified_data)
     if st.sidebar.checkbox("show raw data", False):
         st.write(modified_data)
+
+st.sidebar.subheader("Breakdown airline tweets by sentiment")
+choice = st.sidebar.multiselect('Pick airlines',
+                                ('US Airways', 'United', 'American', 'Southwest', 'Delta', 'Virgin America'), key='0')
+
+if len(choice) > 0:
+    choice_data = data[data.airline.isin(choice)]
+    fig_choice = px.histogram(choice_data, x='airline', y='airline_sentiment', histfunc='count',
+                              color='airline_sentiment', facet_col='airline_sentiment',
+                              labels={'airline_sentiment': 'tweets'}, height=600, width=800)
+    st.plotly_chart(fig_choice)
